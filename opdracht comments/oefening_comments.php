51f1f1b25f180a294f9de807e34c08881c29a4b6
filipha <LocalAdmin@MECH-B107-13.syntraab.mechelen>
2015-08-10 13:00:54 +0200

    opdracht comment

---
+++ opdracht comments/oefening_comments.php
@@ -0,0 +1,12 @@
+<!doctype HTML>
+<html>
+<head>
+	<title>Dit is de titel</title>
+</head>
+<body>
+	<p>naam : <?php echo 'Haesen' ?></p>
+	<p>voornaam : <?= 'Filip' ?></p>
+	
+	<!-- email : filip.haesen@telenet.be -->
+</body>
+</html>
